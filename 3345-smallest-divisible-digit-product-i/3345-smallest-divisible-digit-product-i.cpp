489de73836d4040d544eeb23338b382dcc984e3a
KarthiK <karthik94489@gmail.com>
2026-08-08 00:15:10 +0530

    Time: 0 ms (100%), Space: 8.5 MB (73.1%) - LeetHub

--- 3345-smallest-divisible-digit-product-i/3345-smallest-divisible-digit-product-i.cpp
+++ 3345-smallest-divisible-digit-product-i/3345-smallest-divisible-digit-product-i.cpp
@@ -1,6 +1,21 @@
 class Solution {
 public:
     int smallestNumber(int n, int t) {
-        
+        for(;;){
+        int x=n,y, pro=1;;
+        while(x>0)
+        {
+            y=x%10;
+            pro*=y;
+            x=x/10;
+        }
+        if(pro%t!=0){
+            n++;
+        }
+        else{
+            return n;
+        }
+        }
+        return -1;
     }
 };
